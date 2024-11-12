4d6b5cdea3f3d3d4cc076ef7864039e7c28bf12e
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-11-12 13:35:38 -0500

    env example updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
 import { EmpleadosModule } from './empleados/empleados.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({}),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3307,
+      port: +process.env.DB_PORT,
       //port: 3306,
-      username: 'admin',
-      password: 'admin009',
-      database: 'talentohumanosin_posesio',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
