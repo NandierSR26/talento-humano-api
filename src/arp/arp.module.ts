7fe03faff319be07c31c68e774f22e50886d9d0e
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-11-18 10:07:17 -0500

    ARrp module updated

--- src/arp/arp.module.ts
+++ src/arp/arp.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArpService } from './arp.service';
 import { ArpController } from './arp.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Arp } from './entities/arp.entity';
 
 @Module({
   controllers: [ArpController],
   providers: [ArpService],
+  imports: [TypeOrmModule.forFeature([Arp])],
+  exports: [TypeOrmModule],
 })
 export class ArpModule {}
